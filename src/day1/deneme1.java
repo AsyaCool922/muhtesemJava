bc4c75e55893a3aa1c08fab5c313923749c03d5a
asya <asyacool922@gmail.com>
2023-11-03 17:56:09 +0300

    first commit

---
+++ src/day1/deneme1.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class deneme1 {
+    public static void main(String[] args) {
+        System.out.println("deneme1");
+    }
+
+    public static class deneme2 {
+        public static void main(String[] args) {
+            System.out.println("deneme2");
+        }
+    }
+
+    public static class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+        }
+    }
+}
